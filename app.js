054b7a5cd3dcab55f3c75c51e6895281f0f46868
ckaynatividad <natividad.ckay@gmail.com>
2021-11-01 10:49:21 -0700

    button event listener

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', ()=>{
+    const selected = document.querySelectorAll('input[type=radio]:checked');
+
+    if (selected.length > 3 || selected.length < 3) {
+        alert('Please only choose three.');
+        window.location.reload();
+    }
+});
