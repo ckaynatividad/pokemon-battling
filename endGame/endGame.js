31f7526bdac1d6c9a4ee08f186434439efab128c
Brevan Bender <bcaybender@gmail.com>
2021-11-01 10:35:28 -0700

    results page functional

---
+++ endGame/endGame.js
@@ -0,0 +1,16 @@
+import { getPokemon } from '../functions/utils.js';
+import { isDead } from '../functions/isDead.js';
+import { resultsDisplay, messages } from './endGameLogic.js';
+
+//get pokemon and check if any are alive(isDead)
+const pokes = getPokemon();
+//pokes form local storage
+console.log(pokes);
+//linking to area in html
+const resultsPlace = document.getElementById('main-container');
+//check if the pokemon are alice see endGameLogic.js
+const results = resultsDisplay();
+//call text from js
+const resultsMessage = messages[results];
+//set in html
+resultsPlace.textContent = resultsMessage;
