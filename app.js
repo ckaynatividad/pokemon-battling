2c133784874f937bee6b4dd3d9e8eb23e5a1a5f8
ckaynatividad <natividad.ckay@gmail.com>
2021-11-01 15:13:31 -0700

    testing value

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-import { generatePokemon } from './functions/utils.js';
+import { pokeDex } from './data/pokemon-data.js';
+import findById from './functions/findById.js';
+import { generatePokemon, setPokemon } from './functions/utils.js';
 
 generatePokemon();
 
@@ -6,11 +8,16 @@ const submit = document.getElementById('submit');
 
 submit.addEventListener('click', ()=>{
     const selected = document.querySelectorAll('input[type=radio]:checked');
+    console.log(selected);
+    const huwwo = findById(pokeDex, selected.values);
+    console.log(huwwo);
 
     if (selected.length > 3 || selected.length < 3) {
         alert('Please only choose three.');
-        window.location.reload();
+        // window.location.reload();
     } else {
-        window.location = './battle';
+  
+        setPokemon(selected);
+        // window.location = './battle';
     }
 });
