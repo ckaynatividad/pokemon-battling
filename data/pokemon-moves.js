071b68802aad961ce8944702cf7328d0649e557e
ckaynatividad <natividad.ckay@gmail.com>
2021-11-03 14:10:46 -0700

    meow

--- data/pokemon-moves.js
+++ data/pokemon-moves.js
@@ -1588,41 +1588,38 @@ export let moves = {
         contestType: 'Beautiful',
     },
 
-        dazzlinggleam: {
-            num: 605,
-            accuracy: 100,
-            basePower: 80,
-            category: 'Special',
-            name: 'Dazzling Gleam',
-            pp: 10,
-            priority: 0,
-            flags: { protect: 1, mirror: 1 },
-            secondary: null,
-            target: 'allAdjacentFoes',
-            type: 'Fairy',
-            contestType: 'Beautiful',
-        },
-
-        energyball: {
-            num: 412,
-            accuracy: 100,
-            basePower: 90,
-            category: 'Special',
-            name: 'Energy Ball',
-            pp: 10,
-            priority: 0,
-            flags: { bullet: 1, protect: 1, mirror: 1 },
-            secondary: {
-                chance: 10,
-                boosts: {
-                    spd: -1,
-                },
-            },
-        },
-        target: 'normal',
-        type: 'Grass',
+    dazzlinggleam: {
+        num: 605,
+        accuracy: 100,
+        basePower: 80,
+        category: 'Special',
+        name: 'Dazzling Gleam',
+        pp: 10,
+        priority: 0,
+        flags: { protect: 1, mirror: 1 },
+        secondary: null,
+        target: 'allAdjacentFoes',
+        type: 'Fairy',
         contestType: 'Beautiful',
     },
+
+    energyball: {
+        num: 412,
+        accuracy: 100,
+        basePower: 90,
+        category: 'Special',
+        name: 'Energy Ball',
+        pp: 10,
+        priority: 0,
+        flags: { bullet: 1, protect: 1, mirror: 1 },
+        secondary: {
+            chance: 10,
+            boosts: {
+                spd: -1,
+            },
+        }
+    },
+
 
     psystrike: {
         num: 540,
@@ -1848,5 +1845,5 @@ export let moves = {
         target: 'normal',
         type: 'Grass',
         contestType: 'Tough',
-    },
+    }
 };
